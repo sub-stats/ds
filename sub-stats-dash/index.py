66d142eadb1deedf023500a34bf1467122e27643
Will Haeck <will.haeck@gmail.com>
2019-07-31 12:36:08 -0700

    updated graph

--- sub-stats-dash/index.py
+++ sub-stats-dash/index.py
@@ -1,27 +1,79 @@
-import os
+# -*- coding: utf-8 -*-
+import dash
+import datetime
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
+import plotly.io as pio
+from dash.dependencies import Input, Output
+
+from plotly.graph_objs import *
 
 from app import app, server
 
-def display_plot():
-    df = pd.read_csv(
+df = pd.read_csv(
     'https://raw.githubusercontent.com/sub-stats/ds/master/past_90_days_post_per_day.csv')
 
-    df = df[df['subreddit'] == 'askreddit']
 
-    fig = px.line(df, x='date', y='posts_count')
-    return fig
+df = df[df['subreddit'] == "askreddit"]
 
-fig = display_plot()
-app.layout = html.Div([
-    dcc.Graph(id='submissions-per-day',
-              figure=fig)
-])
+
+##Can do it this way to display all subs on 1 graph
+# fig = px.line(df, x='date', y='posts_count', color='subreddit', template="plotly_dark",
+#     line_shape="spline")
+
+layout = Layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+
+
+fig = go.Figure(layout=layout)
+fig.add_trace(go.Scatter(x=df['date'], y=df['posts_count'],
+                    mode='lines',
+                    name='lines',
+                    line_shape='spline'
+                    ))
+
+
+fig.update_layout(
+    title=go.layout.Title(
+        text="Posts per Day Over Time",
+        xref="paper",
+        x=0.5
+
+    ),
+    xaxis=go.layout.XAxis(
+        title=go.layout.xaxis.Title(
+            text="Date",
+            font=dict(
+                family="Courier New, monospace",
+                size=18,
+                color="#FF4500"
+            )
+        )
+    ),
+    yaxis=go.layout.YAxis(
+        title=go.layout.yaxis.Title(
+            text="Posts per Day",
+            font=dict(
+                family="Courier New, monospace",
+                size=18,
+                color="#FF4500"
+            )
+        )
+    )
+)
+
+fig.update_xaxes(showgrid=False, zeroline=False)
+fig.update_yaxes(showgrid=False, zeroline=False)
+
+fig.show()
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
